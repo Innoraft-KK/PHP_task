688de54b69e4f15a5a94b7f44b125a7bec9e53a5
Innoraft-KK <118758331+Innoraft-KK@users.noreply.github.com>
2023-02-23 11:51:23 +0530

    docblock added to task2.php

--- TASK2/task2.php
+++ TASK2/task2.php
@@ -1,9 +1,52 @@
 <?php
-/* student class created */
+/**
+ * A class representing a student.
+ */
 class student{
 
-        public $fname,$lname,$fullname,$img,$img_temp;
+         /**
+     * The student's first name.
+     *
+     * @var string
+     */
+    public $fname;
 
+    /**
+     * The student's last name.
+     *
+     * @var string
+     */
+    public $lname;
+
+    /**
+     * The student's full name.
+     *
+     * @var string
+     */
+    public $fullname;
+     /**
+     * The name of the student's image file.
+     *
+     * @var string
+     */
+    public $img;
+
+    /**
+     * The temporary location of the student's image file.
+     *
+     * @var string
+     */
+    public $img_temp;
+     /**
+     * Creates a new instance of the student class.
+     *
+     * @param string $first The student's first name.
+     * @param string $last The student's last name.
+     * @param string $img_name The name of the student's image file.
+     * @param string $img_t The temporary location of the student's image file.
+     * 
+     * @return void
+     */
         function __construct($first,$last,$img_name,$img_t){
             $this->fname = $first;
             $this->lname = $last;
@@ -11,7 +54,11 @@ class student{
             $this->img=$img_name;
             $this->img_temp=$img_t;
         }
-
+         /**
+     * Outputs a message related to the student's name.
+     * 
+     * @return void
+     */
         function message(){//name related message
             if (!preg_match('/^[a-zA-Z]+$/', $this->fname)){
                 echo "Invalid Input in First Name";
@@ -23,8 +70,12 @@ class student{
                 echo "Hello ".$this->fullname;
             }
         }
-
-        function img_msg(){//image related message
+        /**
+     * Outputs a message related to the student's image file and moves the file to a specified directory.
+     *
+     * @return void
+     */
+        function img_msg(){
                 if(move_uploaded_file($this->img_temp,"upload-images/".$this->img)){
                     echo "<br> Successfully uploaded";
                     echo "<img src='./upload-images/".$this->img."'>";
