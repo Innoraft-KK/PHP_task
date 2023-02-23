694f1465e6feb5242e75d64215e674e8d8fbb9be
Innoraft-KK <118758331+Innoraft-KK@users.noreply.github.com>
2023-02-23 11:38:33 +0530

    docblock added to task1

--- TASK1/task1.php
+++ TASK1/task1.php
@@ -1,13 +1,38 @@
 <?php
-/* student class created */
+/**
+ * Represents a student with a first name, last name, and full name.
+ */
 class student{
 
-    public $fname,$lname,$fullname;
+    /**
+     * @var string $fname The first name of the student.
+     */
+    public $fname;
+
+    /**
+     * @var string $lname The last name of the student.
+     */
+    public $lname;
+
+    /**
+     * @var string $fullname The full name of the student, consisting of the first and last names.
+     */
+    public $fullname;
+
+    /**
+     * Constructs a new instance of the student class with the given first and last names.
+     *
+     * @param string $first The first name of the student.
+     * @param string $last The last name of the student.
+     */
     function __construct($first,$last){
         $this->fname = $first;
         $this->lname = $last;
         $this->fullname = $this->fname .' '. $this->lname;
     }
+     /**
+     * Outputs a message containing the full name of the student, or an error message if the first or last name is invalid.
+     */
     function message(){//name related message
     if (!preg_match('/^[a-zA-Z]+$/', $this->fname)){
         echo "Invalid Input in First Name";
@@ -20,7 +45,9 @@ class student{
     }
     }
 }
-/* student object initiated if first and last name are entered*/
+/**
+ * Creates a new instance of the student class and outputs a message if valid first and last names are provided via POST.
+ */
 if(isset($_POST['first_name']) and isset($_POST['last_name'])){
     $f = $_POST['first_name'];
     $l = $_POST['last_name'];
