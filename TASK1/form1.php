4c659fe4519a543cee3535bc97c4221917e7e38c
Innoraft-KK <118758331+Innoraft-KK@users.noreply.github.com>
2023-03-08 02:24:30 +0530

    Update form1.php

--- TASK1/form1.php
+++ TASK1/form1.php
@@ -14,11 +14,11 @@
         <form method="post">
             <div>
                 <label for="first_name">First Name</label>
-                <input type="text" id="first_name" name="first_name" required onchange="updateFullName()"><br>
+                <input type="text" id="first_name" name="first_name" required oninput="updateFullName()"><br>
             </div>
             <div>
                 <label for="last_name">Last Name</label>
-                <input type="text" id="last_name" name="last_name" required onchange="updateFullName()"><br>
+                <input type="text" id="last_name" name="last_name" required oninput="updateFullName()"><br>
             </div>    
             <div>
                 <label for="full_name">Full Name</label>
