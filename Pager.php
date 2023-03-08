aee12b6fdf83c2f8e0a3fba8c2c1ee76440d59e3
Innoraft-KK <118758331+Innoraft-KK@users.noreply.github.com>
2023-03-08 11:08:19 +0530

    Update Pager.php

--- Pager.php
+++ Pager.php
@@ -3,22 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-.width, initial-scale=1.0">
+    <link rel="stylesheet" href="pager_style.css">
     <title>Index of Task</title>
 </head>
 <body>
-    <ul>
-        <li><a href="Pager.php?form=1">Task 1</a></li>
-        <li><a href="Pager.php?form=2">Task 2</a></li>
-        <li><a href="Pager.php?form=3">Task 3</a></li>
-        <li><a href="Pager.php?form=4">Task 4</a></li>
-        <li><a href="Pager.php?form=5">Task 5</a></li>
-        <li><a href="Pager.php?form=6">Task 6</a></li>
-    </ul>
-    <form method="post">
+    
+<header>
+    <h2>PHP Task</h2>
+    <div>
+        <a href="Pager.php?form=1" class="active">Task 1</a>
+        <a href="Pager.php?form=2" class="active">Task 2</a>
+        <a href="Pager.php?form=3" class="active">Task 3</a>
+        <a href="Pager.php?form=4" class="active">Task 4</a>
+        <a href="Pager.php?form=5" class="active">Task 5</a>
+        <a href="Pager.php?form=6" class="active">Task 6</a>
+    </div> 
+    <form method="post" class='logout'>
         <input type="submit" value="Log Out" name="logout">
     </form>
-    <?php include 'pager_logic.php';?>
-    
+</header>
+   <?php  include 'pager_logic.php';?> 
 </body>
 </html>
