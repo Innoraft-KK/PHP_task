241f0b2e78e91433d306780606bcc8279b83f5ae
Innoraft-KK <118758331+Innoraft-KK@users.noreply.github.com>
2023-03-08 09:04:36 +0530

    Update form2.php

--- TASK2/form2.php
+++ TASK2/form2.php
@@ -14,24 +14,26 @@
         <form method="post" enctype="multipart/form-data">
             <div>
                 <label for="first_name">First Name</label>
-                <input type="text" id="first_name" name="first_name" required onchange="updateFullName()"><br>
+                <input type="text" id="first_name" name="first_name" oninput="updateFullName()" required "><br>
             </div>
             <div>
                 <label for="last_name">Last Name</label>
-                <input type="text" id="last_name" name="last_name" required onchange="updateFullName()"><br>
+                <input type="text" id="last_name" name="last_name" oninput="updateFullName()" required "><br>
             </div>    
             <div>
                 <label for="full_name">Full Name</label>
                 <input type="text" id="full_name" readonly="/readonly" ><br/>
             </div>
-            <div><input type="file" name="image"></div>
-            <input type="submit">
+            <div><input type="file" name="image" accept=".png,.gif,.jpeg"></div>
+            <input type="submit" value='Submit'>
             <br/>
             <!-- php file included --> 
             <?php include ("task2.php");?>
         </form>
+        <!-- scirpt included -->
+        <script type="text/javascript" src="./index.js"></script>
    <!-- form ended -->
    </body>
-    <!-- scirpt included -->
-    <script type="text/javascript" src="index.js"></script>
+
+    
     </html>
