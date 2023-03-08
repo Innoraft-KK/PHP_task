aaeb871b35ef1378038cc593dd9826dd4ec5eb43
Innoraft-KK <118758331+Innoraft-KK@users.noreply.github.com>
2023-03-08 11:07:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <form action="./Login_logic.php" method="post">
-        <div>
-            <label for="user_name">Name</label>
-            <input type="text" name="user_name" id="user_name">
+    <?php
+    session_start();
+    if(($_SESSION['user_name'] == 'kaaarrrtik')):
+        header('Location: Pager.php');
+    
+    else:
+    
+    ?>
+    <form  method="post">
+        <div class='flexme'>
+            <div>
+                <label for="user_name">Name</label>
+                <input type="text" name="user_name" id="user_name" placeholder="Name">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Password">
+            </div>
+            <input type="submit" value="Login">
         </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Password">
-        </div>
-        <input type="submit" value="Login">
-        <?php include "./Login_logic.php" ?>
+        <?php
+        include "./Login_logic.php";  
+        endif;
+        ?>
     </form>
+
 </body>
 </html>
